ed8e90a6ff014ee3407ced702abcc6b5f294e2b1
pauljay611 <pauljay611@gmail.com>
2020-03-29 19:53:57 +0800

    add copy and clean webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const CopyPlugin = require('copy-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
 	mode: 'production',
@@ -9,5 +11,19 @@ module.exports = {
 	output: {
 		filename: '[name].bundle.js',
 		path: path.resolve(__dirname, 'chrome')
-	}
+	},
+
+	plugins: [
+		new CleanWebpackPlugin(),
+		new CopyPlugin([
+			{
+				from: './src/manifest.json',
+				to: path.resolve(__dirname, 'chrome')
+			},
+			{
+				from: './src/images',
+				to: path.resolve(__dirname, 'chrome/images')
+			}
+		])
+	]
 }
